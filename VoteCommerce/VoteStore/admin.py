80eb53ba5d557ed6cad4d212e99dbb884c987abd
bukeze2048 <bukeze2119@outlook.com>
2023-06-06 23:00:12 +0100

    updated admin

--- VoteCommerce/VoteStore/admin.py
+++ VoteCommerce/VoteStore/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Contestant
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(Contestant)
+class ContestantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'votes')
+    list_filter = ('category',)
+    search_fields = ('name',)
+    readonly_fields = ('votes',)
+
+admin.site.site_header = 'VoteStore Administration'
+admin.site.site_title = 'VoteStore Admin'
