d8abb40527f586167efedbe3ac7fa54e98ade236
bukeze2048 <bukeze2119@outlook.com>
2023-06-06 22:30:55 +0100

    first models

--- VoteCommerce/VoteStore/models.py
+++ VoteCommerce/VoteStore/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+class Contestant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='contestant_images')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    votes = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
